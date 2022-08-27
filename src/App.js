c423e5de3bf72ec11f2198c41151530cda07a899
dhachanamoorthy <dhachanamoorthy3@gmail.com>
2022-08-27 22:47:31 +0530

    feat : add nav bar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import { Header } from "./components/Header/Header";
+import { NavBar } from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import "./NavBar.css";
+export const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <a className="navbar-links" href="#">
+        REPORT
+      </a>
+      <a className="navbar-links" href="#">
+        ADD
+      </a>
+      <a className="navbar-links" href="#">
+        SETTINGS
+      </a>
+    </nav>
+  );
+};
