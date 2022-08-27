1b51f9384b8ebbf98217c547ec43d0db1967854f
dhachanamoorthy <dhachanamoorthy3@gmail.com>
2022-08-27 21:20:02 +0530

    feat : add app header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import "./Header.css";
+export const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">PettyCa$H</div>
+      <div className="profile"></div>
+    </header>
+  );
+};
