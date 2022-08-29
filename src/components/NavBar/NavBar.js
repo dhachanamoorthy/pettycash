f798695616238e2adc50ee654690cf66f423fd8d
dhachanamoorthy <dhachanamoorthy3@gmail.com>
2022-08-29 17:26:35 +0530

    chore : rework on navbar and colors

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,19 @@
 import "./NavBar.css";
+import AssessmentIcon from '@mui/icons-material/Assessment';
+import SettingsIcon from '@mui/icons-material/Settings';
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
 export const NavBar = () => {
   return (
     <nav className="navbar">
-      <a className="navbar-links" href="#">
-        REPORT
-      </a>
-      <a className="navbar-links" href="#">
-        ADD
-      </a>
-      <a className="navbar-links" href="#">
-        SETTINGS
-      </a>
+      <div className="navbar-links" >
+        <AssessmentIcon className="navbar-icon" />
+      </div>
+      <div className="navbar-links">
+        <MonetizationOnIcon className="navbar-icon" />
+      </div>
+      <div className="navbar-links">
+        <SettingsIcon className="navbar-icon" />
+      </div>
     </nav>
   );
 };
