93853ee18a586de610be9a4470f5ac6698cd5e39
dhachanamoorthy <dhachanamoorthy3@gmail.com>
2022-08-30 20:14:14 +0530

    feat : add 404 error component

---
+++ src/components/Error/404.js
@@ -0,0 +1,8 @@
+import asset_404 from '../../assets/404.png';
+export const Error_404 = () => {
+    return (
+        <div className="error">
+            <img className="error-asset" src={asset_404} />
+        </div>
+    )
+}
