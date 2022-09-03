ed3e999af8c47e889398730768391a6b4db3abe5
dhachanamoorthy <dhachanamoorthy3@gmail.com>
2022-09-03 23:04:19 +0530

    feat : add routers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Home } from "./pages/Home/Home";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { Add } from "./pages/Home/Add";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="/report" element={<Home />} />
+          <Route path="/settings" element={<Home />} />
+          <Route path="add" element={<Add />} />
+          {/* <Route path="teams" element={<Teams />}>
+            <Route path=":teamId" element={<Team />} />
+            <Route path="new" element={<NewTeamForm />} />
+            <Route index element={<LeagueStandings />} />
+          </Route> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
     <App />
   </React.StrictMode>
 );
