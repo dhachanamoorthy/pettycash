142a18c76b32c70ec622d22c0891d3a462d9a2b1
dhachanamoorthy <dhachanamoorthy3@gmail.com>
2022-09-03 23:09:26 +0530

    feat : add plain components

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import SpeedDial from "@mui/material/SpeedDial";
 import AddIcon from "@mui/icons-material/Add";
+import { Link } from "react-router-dom";
 import "./Home.css";
 import { Container, Paper, Divider } from "@mui/material";
 export const Home = () => {
@@ -18,12 +19,14 @@ export const Home = () => {
         <div className="value">$40</div>
       </Paper>
       <Divider />
+      <Link to="/add">
       <SpeedDial
         ariaLabel="SpeedDial basic example"
         sx={{ position: "absolute", bottom: 72, right: 16 }}
         icon={<AddIcon />}
         onClick={handleclick}
       ></SpeedDial>
+      </Link>
     </Container>
   );
 };
--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -1,8 +1,7 @@
-
 export const Report=()=>{
     return(
         <div>
-            Report
+            Settings
         </div>
     )
 }
