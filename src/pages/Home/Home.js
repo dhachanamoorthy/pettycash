5315833e8ea1fd084b96d0a24228a3d135eb3720
dhachanamoorthy <dhachanamoorthy3@gmail.com>
2022-09-11 22:56:28 +0530

    chore : reformat code

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,29 +4,29 @@ import { Link } from "react-router-dom";
 import "./Home.css";
 import { Container, Paper, Divider } from "@mui/material";
 export const Home = () => {
-  const handleclick = () => {
-    console.log("clicked");
-  };
+    const handleclick = () => {
+        console.log("clicked");
+    };
 
-  return (
-    <Container sx={{ "& > :not(style)": { m: 1 } }}>
-      <Paper className="card income" sx={{ "& > :not(style)"  : { m: 1 } }}>
-        <div className="name">Income</div>
-        <div className="value">$40</div>
-      </Paper>
-      <Paper className="card expense" sx={{ "& > :not(style)": { m: 1 } }}>
-        <div className="name">Expense</div>
-        <div className="value">$40</div>
-      </Paper>
-      <Divider />
-      <Link to="/add">
-      <SpeedDial
-        ariaLabel="SpeedDial basic example"
-        sx={{ position: "absolute", bottom: 82, right: 20 }}
-        icon={<AddIcon />}
-        onClick={handleclick}
-      ></SpeedDial>
-      </Link>
-    </Container>
-  );
+    return (
+        <Container sx={{ "& > :not(style)": { m: 1 } }}>
+            <Paper className="card income" sx={{ "& > :not(style)": { m: 1 } }}>
+                <div className="name">Income</div>
+                <div className="value">$40</div>
+            </Paper>
+            <Paper className="card expense" sx={{ "& > :not(style)": { m: 1 } }}>
+                <div className="name">Expense</div>
+                <div className="value">$40</div>
+            </Paper>
+            <Divider />
+            <Link to="/add">
+                <SpeedDial
+                    ariaLabel="SpeedDial basic example"
+                    sx={{ position: "absolute", bottom: 82, right: 20 }}
+                    icon={<AddIcon />}
+                    onClick={handleclick}
+                ></SpeedDial>
+            </Link>
+        </Container>
+    );
 };
